82ab798b1eb531c47e7f4c95a6deec93833adbfd
Edgar Agno <edgardo.agno@gmail.com>
2025-07-31 11:59:56 +0100

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by EDGARDO AGNO on 31/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
