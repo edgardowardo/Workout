f93b51554b5647c45a35c39fff884aee740b4aec
Edgar Agno <edgardo76@yahoo.com>
2025-08-04 20:10:27 +0100

    WorkoutEnums

---
+++ Workout/Models/WorkoutEnums.swift
@@ -0,0 +1,26 @@
+
+enum RestTimeSeconds: Int, CaseIterable, CustomStringConvertible {
+    case short = 30
+    case medium = 60
+    case long = 120
+    
+    var description: String {
+        switch self {
+        case .short:
+            return "30 sec"
+        case .medium:
+            return "1 min"
+        case .long:
+            return "2 min"
+        }
+    }
+}
+
+enum WorkoutStage {
+    case initial
+    case started
+    case typing
+    case picker
+    case resting
+    case summary
+}
